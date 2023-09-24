33ed75d0a515102bcf62c63514053ad53955a16c
isri12 <itsehayu12@gmail.com>
2023-09-24 17:50:10 -0400

    added factorial and gtestfactorial

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "include/subtraction.h"
 #include "include/multiplication.h"
 #include "include/division.h"
-
+#include "include/factorial.h"
 
 int main() {
     double num1, num2;
@@ -12,41 +12,43 @@ int main() {
     std::cout << "Simple Calculator" <<std::endl;
     std::cout << "------------------" <<std::endl;
 
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
+    // std::cout << "Enter first number: ";
+    // std::cin >> num1;
 
-    std::cout << "Enter an operator (+, -, *, /): ";
-    std::cin >> operation;
+    int result = factorial(3);
 
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
+    // std::cout << "Enter an operator (+, -, *, /): ";
+    // std::cin >> operation;
 
-    double result;
+    // std::cout << "Enter second number: ";
+    // std::cin >> num2;
 
-    Addition a1;
+    // double result;
 
-    switch (operation) {
-        case '+':
-            result = a1.add(num1, num2);
-            break;
-        case '-':
-            result = subtract(num1, num2);
-            break;
-        case '*':
-            result = multiply(num1, num2);
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = divide(num1, num2);
-            } else {
-                std::cout << "Error: Division by zero!" << std::endl;
-                return 1; // Exit with an error code
-            }
-            break;
-        default:
-            std::cout << "Invalid operator!" << std::endl;
-            return 1; // Exit with an error code
-    }
+    // Addition a1;
+
+    // switch (operation) {
+    //     case '+':
+    //         result = a1.add(num1, num2);
+    //         break;
+    //     case '-':
+    //         result = subtract(num1, num2);
+    //         break;
+    //     case '*':
+    //         result = multiply(num1, num2);
+    //         break;
+    //     case '/':
+    //         if (num2 != 0) {
+    //             result = divide(num1, num2);
+    //         } else {
+    //             std::cout << "Error: Division by zero!" << std::endl;
+    //             return 1; // Exit with an error code
+    //         }
+    //         break;
+    //     default:
+    //         std::cout << "Invalid operator!" << std::endl;
+    //         return 1; // Exit with an error code
+    // }
 
     std::cout << "Result: " << result << std::endl;
 
---
+++ src/factorial.cpp
@@ -0,0 +1,10 @@
+#include "../include/factorial.h"
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    } else
+    return n * factorial(n - 1);
+}
